ba4a508919d804bcabcc997a8256b8cc1c013654
codinci <dkibau@gmail.com>
2022-08-30 20:52:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedString = reverseString(word);
+
+  if(word === reversedString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-/* 
+function reverseString(word) {
+  //create an array from the input string
+  const wordArray = word.split('');
+  //reverse the array
+  const reversedWordArray = wordArray.reverse();
+  //create a string from the reversed array
+  const reversedWord = reversedWordArray.join('');
+  //return the reversed string
+  return reversedWord;
+}
+/*
   Add your pseudocode here
+  Reverse the input string
+
+  If the reversed input string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
